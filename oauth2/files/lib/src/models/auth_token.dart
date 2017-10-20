9dba6eea520950d2d9687e7e80988a498b112f4f
Tobe O <thosakwe@gmail.com>
2017-10-20 12:56:08 -0400

    Update auth_token.dart

--- oauth2/files/lib/src/models/auth_token.dart
+++ oauth2/files/lib/src/models/auth_token.dart
@@ -2,7 +2,6 @@ library {{ project_name }}.src.models.auth_token;
 
 import 'package:angel_model/angel_model.dart';
 import 'package:angel_serialize/angel_serialize.dart';
-import 'application.dart';
 part 'auth_token.g.dart';
 
 @serializable
@@ -10,8 +9,6 @@ class _AuthToken extends Model {
   String userId, applicationId, state, refreshToken;
   List<String> scopes;
   int lifeSpan;
-
-  Application application;
 
   bool get expired {
     var now = new DateTime.now().toUtc();
